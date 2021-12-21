063acf8658186edca2466fdca952e05bc0041a79
1313ou <1313ou@gmail.com>
2021-12-21 10:31:29 +0100

    revised serialization/deserialization

--- src/main/java/org/oewntk/sql/out/DeSerializeNIDs.java
+++ src/main/java/org/oewntk/sql/out/DeSerializeNIDs.java
@@ -57,4 +57,20 @@ public class DeSerializeNIDs
 			return ois.readObject();
 		}
 	}
+
+	static public void main(String[] args) throws IOException, ClassNotFoundException
+	{
+		File inDir = new File(args[0]);
+		if (!inDir.isDirectory())
+		{
+			System.exit(1);
+		}
+
+		Map<String, Map<String, Integer>> maps = deserializeNIDs(inDir);
+		System.out.printf("%s %d%n", Names.WORDS.FILE, maps.get(Names.WORDS.FILE).size());
+		System.out.printf("%s %d%n", Names.CASEDWORDS.FILE, maps.get(Names.CASEDWORDS.FILE).size());
+		System.out.printf("%s %d%n", Names.MORPHS.FILE, maps.get(Names.MORPHS.FILE).size());
+		System.out.printf("%s %d%n", Names.PRONUNCIATIONS.FILE, maps.get(Names.PRONUNCIATIONS.FILE).size());
+		System.out.printf("%s %d%n", Names.SYNSETS.FILE, maps.get(Names.SYNSETS.FILE).size());
+	}
 }
