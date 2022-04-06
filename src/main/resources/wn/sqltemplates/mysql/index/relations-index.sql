14ce72c14d6fef9098f89efec12516dcfe15f94d
1313ou <1313ou@gmail.com>
2022-04-06 16:34:05 +0200

    fix sqlite syntax

--- src/main/resources/wn/sqltemplates/mysql/index/relations-index.sql
+++ src/main/resources/wn/sqltemplates/mysql/index/relations-index.sql
@@ -1,2 +1,2 @@
 ALTER TABLE ${relations.table} ADD CONSTRAINT `pk_@{relations.table}`                       PRIMARY KEY  (${relations.relationid});
-ALTER TABLE ${relations.table} ADD CONSTRAINT `uk_@{relations.table}_@{relations.relation}` UNIQUE INDEX (${relations.relation});
+ALTER TABLE ${relations.table} ADD CONSTRAINT `uk_@{relations.table}_@{relations.relation}` UNIQUE KEY   (${relations.relation});
