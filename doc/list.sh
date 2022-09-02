6c4c5b22a0be6788cb0426b471c62e6945991479
1313ou <1313ou@gmail.com>
2022-09-02 10:28:20 +0200

    doc

---
+++ doc/list.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export umls="uml-wordnet1 uml-wordnet2"
+export schemas="schemas-wordnet1 schemas-wordnet2"
+export fks="fks-wordnet"
+export tables="tables-wordnet"
+
+export files="${umls} ${schemas} ${tables} ${fks}"
+export domains="wordnet"
