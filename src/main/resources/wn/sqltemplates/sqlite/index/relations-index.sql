07281464110a696e8016de4a9a8914e65e1edccd
1313ou <1313ou@gmail.com>
2022-09-03 18:51:15 +0200

    fix sqlite syntax

--- src/main/resources/wn/sqltemplates/sqlite/index/relations-index.sql
+++ src/main/resources/wn/sqltemplates/sqlite/index/relations-index.sql
@@ -1,2 +1,2 @@
 CREATE UNIQUE INDEX `pk_@{relations.table}` ON ${relations.table} (${relations.relationid});
-ALTER TABLE ${relations.table} ADD CONSTRAINT `uk_@{relations.table}_@{relations.relation}` UNIQUE INDEX (${relations.relation});
+CREATE UNIQUE INDEX `uk_@{relations.table}_@{relations.relation}` ON ${relations.table} (${relations.relation});
