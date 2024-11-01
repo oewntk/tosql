322b0bcab29dfe546006a28c3702812d685402ca
1313ou <1313ou@gmail.com>
2024-11-01 07:27:07 +0100

    add default to null

--- src/main/resources/wn/sqltemplates/sqlite/create/lexes-create.sql
+++ src/main/resources/wn/sqltemplates/sqlite/create/lexes-create.sql
@@ -2,5 +2,5 @@ CREATE TABLE ${lexes.table} (
 ${lexes.luid} INT NOT NULL,
 ${lexes.posid} CHARACTER (1) CHECK( ${lexes.posid} IN ('n','v','a','r','s') ) NOT NULL,
 ${lexes.wordid} INT NOT NULL,
-${lexes.casedwordid} INT NULL
+${lexes.casedwordid} INT DEFAULT NULL
 );
