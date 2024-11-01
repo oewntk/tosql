b3a57cf8f3da70fd059273bdcadea3e6a48c8b41
1313ou <1313ou@gmail.com>
2024-11-01 07:26:55 +0100

    add default to null

--- src/main/resources/wn/sqltemplates/mysql/create/lexes-create.sql
+++ src/main/resources/wn/sqltemplates/mysql/create/lexes-create.sql
@@ -2,5 +2,5 @@ CREATE TABLE ${lexes.table} (
 ${lexes.luid}        INT                       NOT NULL,
 ${lexes.posid}       ENUM('n','v','a','r','s') NOT NULL,
 ${lexes.wordid}      INT                       NOT NULL,
-${lexes.casedwordid} INT                       NULL
+${lexes.casedwordid} INT                       DEFAULT NULL
 );
